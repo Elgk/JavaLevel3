9263a013632e585a24d1528cfdabfd2747297744
Elgk <lena.elena-kuznetzova2016@yandex.ru>
2021-03-09 13:49:24 +0400

    Homework 1

---
+++ src/lesson1/hwFruitBox/Apple.java
@@ -0,0 +1,10 @@
+package lesson1.hwFruitBox;
+
+public class Apple extends Fruit{
+    private final float weight = 1.0f;
+
+    @Override
+    public float getWeight() {
+        return weight;
+    }
+}
---
+++ src/lesson1/hwFruitBox/Orange.java
@@ -0,0 +1,10 @@
+package lesson1.hwFruitBox;
+
+public class Orange extends Fruit{
+    private final float weight = 1.5f;
+
+    @Override
+    public float getWeight() {
+        return weight;
+    }
+}
